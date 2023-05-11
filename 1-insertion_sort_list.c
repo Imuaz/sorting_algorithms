465932515b7da6139de07f8e086c14219b4d8359
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-11 21:10:09 +0000

    readme review and makeup

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,38 +1,36 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sort a double linked list with
- * Insertion sort algorithm
- * @list: double pointer to the list
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       using the insertion sort algorithm.
+ * @list: A pointer to the head of a doubly-linked list of integers.
+ *
+ * Description: Prints the list after each swap.
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *aux;
-	listint_t *aux2 = *list;
-	listint_t *i = *list;
+	listint_t *iter, *insert;
 
-	if (aux2 == NULL)
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	aux2 = aux2->next;
-	while (aux2 != NULL)
+	for (iter = (*list)->next; iter != NULL; iter = iter->next)
 	{
-		i = aux2;
-		aux2 = aux2->next;
-		while (i->prev != NULL && i->n < i->prev->n)
+		insert = iter->prev;
+		while (insert != NULL && iter->n < insert->n)
 		{
-			if (i->next != NULL)
-				i->next->prev = i->prev;
-			if (i->prev->prev != NULL)
-				i->prev->prev->next = i;
-			else
-				*list = i;
-			i->prev->next = i->next;
-			i->next = i->prev;
-			aux = i->prev->prev;
-			i->prev->prev = i;
-			i->prev = aux;
-			print_list(*list);
+			iter->prev = insert->prev;
+			insert->next = iter->next;
+			iter->next = insert;
+			if (insert->prev != NULL)
+				insert->prev->next = iter;
+			insert->prev = iter;
+			if (*list == insert)
+				*list = iter;
+			if (insert->next != NULL)
+				insert->next->prev = insert;
+			print_list((const listint_t *)*list);
+			insert = iter->prev;
 		}
 	}
 }
