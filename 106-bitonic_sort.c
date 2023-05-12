832bf7e4e2d559c9bfbf8be4891d3860eb840bba
Idris Muazu Yakub <imuazyakub@gmail.com>
2023-05-12 13:05:38 +0000

    readme review and makeup

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -1,80 +1,75 @@
 #include "sort.h"
 
-
 /**
- * int_swp - Swap two integers in an array.
+ * swap_ints - Swap two integers in an array.
  * @a: The first integer to swap.
  * @b: The second integer to swap.
  */
-void int_swp(int *a, int *b)
+void swap_ints(int *a, int *b)
 {
-	int tmp = *a;
-	*a = *b;
-	*b = tmp;
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
 }
 
 /**
- * bit_mrg - Sort a bitonic sequence inside an array of integers.
+ * bitonic_merge - Sort a bitonic sequence inside an array of integers.
  * @array: An array of integers.
- * @size: The size of the array.
- * @start: The starting index of the sequence in array to sort.
- * @seq: The size of the sequence to sort.
- * @flow: The direction to sort in.
+ * @start: The starting index of the sequence in the array to sort.
+ * @size: The size of the sequence to sort.
+ * @dir: The direction to sort in (1 for ascending, 0 for descending).
  */
-void bit_mrg(int *array, size_t size, size_t start, size_t seq, char flow)
+void bitonic_merge(int *array, size_t start, size_t size, int dir)
 {
-	size_t i, jump = seq / 2;
-
-	if (seq > 1)
-	{
-		for (i = start; i < start + jump; i++)
-		{
-			if ((flow == UP && array[i] > array[i + jump]) ||
-			    (flow == DOWN && array[i] < array[i + jump]))
-			{
-				int_swp(&array[i], &array[i + jump]);
-				printf("Result [%lu/%lu] (%s):\n", seq, size, flow == UP ? "UP" : "DOWN");
-				print_array(array, size);
-			}
-		}
-		bit_mrg(array, size, start, jump, flow);
-		bit_mrg(array, size, start + jump, jump, flow);
-	}
+    if (size > 1)
+    {
+        size_t mid = size / 2;
+        for (size_t i = start; i < start + mid; i++)
+        {
+            if ((array[i] > array[i + mid] && dir == 1) ||
+                (array[i] < array[i + mid] && dir == 0))
+            {
+                swap_ints(&array[i], &array[i + mid]);
+                printf("Result [%lu/%lu] (%s):\n", size, size, dir == 1 ? "UP" : "DOWN");
+                print_array(array, size);
+            }
+        }
+        bitonic_merge(array, start, mid, dir);
+        bitonic_merge(array, start + mid, mid, dir);
+    }
 }
 
 /**
- * bit_seq - Convert an array of integers into a bitonic sequence.
+ * bitonic_sort_recursive - Recursive helper function for Bitonic sort.
  * @array: An array of integers.
- * @size: The size of the array.
- * @start: The starting index of a block of the building bitonic sequence.
- * @seq: The size of a block of the building bitonic sequence.
- * @flow: The direction to sort the bitonic sequence block in.
+ * @start: The starting index of the sequence in the array to sort.
+ * @size: The size of the sequence to sort.
+ * @dir: The direction to sort in (1 for ascending, 0 for descending).
  */
-void bit_seq(int *array, size_t size, size_t start, size_t seq, char flow)
+void bitonic_sort_recursive(int *array, size_t start, size_t size, int dir)
 {
-	size_t cut = seq / 2;
-
-	if (seq > 1)
-	{
-		bit_seq(array, size, start, cut, UP);
-		bit_seq(array, size, start + cut, cut, DOWN);
-		bit_mrg(array, size, start, seq, flow);
-	}
+    if (size > 1)
+    {
+        size_t mid = size / 2;
+        bitonic_sort_recursive(array, start, mid, 1);
+        bitonic_sort_recursive(array, start + mid, mid, 0);
+        bitonic_merge(array, start, size, dir);
+    }
 }
 
 /**
- * bitonic_sort - Sort an array of integers in ascending
- *                order using the bitonic sort algorithm.
+ * bitonic_sort - Sort an array of integers in ascending order
+ * using the Bitonic sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
  *
  * Description: Prints the array after each swap. Only works for
- * size = 2^k where k >= 0 (ie. size equal to powers of 2).
+ * size = 2^k where k >= 0 (i.e., size equal to powers of 2).
  */
 void bitonic_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
+    if (array == NULL || size < 2)
+        return;
 
-	bit_seq(array, size, 0, size, UP);
+    bitonic_sort_recursive(array, 0, size, 1);
 }
